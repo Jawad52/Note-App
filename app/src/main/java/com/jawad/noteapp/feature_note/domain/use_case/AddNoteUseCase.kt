86a2c884d54ca67acadec5c1105867a27a668e3a
Jawad52 <jawadu992@gmail.com>
2022-03-16 15:05:57 +0400

    Updated AddNote use case for validation

--- app/src/main/java/com/jawad/noteapp/feature_note/domain/use_case/AddNoteUseCase.kt
+++ app/src/main/java/com/jawad/noteapp/feature_note/domain/use_case/AddNoteUseCase.kt
@@ -1,11 +1,19 @@
 package com.jawad.noteapp.feature_note.domain.use_case
 
+import com.jawad.noteapp.feature_note.domain.model.InvalidNoteException
 import com.jawad.noteapp.feature_note.domain.model.Note
 import com.jawad.noteapp.feature_note.domain.repository.NoteRepository
 import javax.inject.Inject
 
 class AddNoteUseCase @Inject constructor(private val noteRepository: NoteRepository) {
+
+    @Throws(InvalidNoteException::class)
     suspend operator fun invoke(note: Note) {
+        if (note.title.isBlank())
+            throw InvalidNoteException("Note title can't be empty")
+        if (note.content.isBlank())
+            throw InvalidNoteException("Note content can't be empty")
+
         noteRepository.insertNote(note)
     }
 }
